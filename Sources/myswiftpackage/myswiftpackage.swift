61c1c3e576b562e51d69c26f5a3b14235e20c5fd
Asia Serrano <asiamserrano@gmail.com>
2024-07-11 13:26:37 -0400

    done

--- Sources/myswiftpackage/myswiftpackage.swift
+++ Sources/myswiftpackage/myswiftpackage.swift
@@ -1,6 +1,10 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-public func foobar() -> Void {
+public func foo() -> Void {
+    print("Hello, World!")
+}
+
+public func bar() -> Void {
     print("Hello, World!")
 }
